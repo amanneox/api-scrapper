1d21d55dff787ac4f2d89069bca834de89c94637
amanadhikari <amanadhikari2@gmail.com>
2018-07-14 15:58:50 +0530

    Added Places API

--- google/handlers/places.js
+++ google/handlers/places.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
-const Promise = require('bluebird');
-const validator = require('validator');
+const mongoose = require('mongoose')
+const Promise = require('bluebird')
+const validator = require('validator')
 require('dotenv').config()
-
+const googlePlaces = require('node-googleplaces')
 const mongoString = process.env.MONGO_URI // MONGO MONGO_URI
-const gooleKey = process.env.GOOGLE_API   // GOOGLE KEY
+const googleKey = process.env.GOOGLE_API   // GOOGLE KEY
 
 const mongoString = process;
+
+const places = new googlePlaces(googleKey)
 
 const createErrorResponse = (statusCode, message) => ({
   statusCode: statusCode || 501,
@@ -18,4 +20,15 @@ const dbExecute = (db, fn) => db.then(fn).finally(() => db.close());
 
 function dbConnectAndExecute(dbUrl, fn) {
   return dbExecute(mongoose.connect(dbUrl, { useMongoClient: true }), fn);
+}
+
+const params = {
+  location: '49.250964,-123.102192',
+  radius: 1000
+}
+
+module.exports.nearbySearch = (event, context, callback) => {
+  places.nearbySearch(query).then((res) => {
+    console.log(res.body)
+  })
 }
